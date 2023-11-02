fa8af20bc37accea3daeb379805cc11314f7afb0
Fatih <fatiheee@icloud.com>
2023-11-02 13:16:02 +0300

    Development of Form Component

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,56 @@
+import {useState, useEffect} from "react";
+
+function Form({addContacts, contacts}) {
+
+    const [form, setForm] = useState({
+        fullname: "",
+        phone_number: ""
+    });
+
+    useEffect(() => {
+        setForm({ fullname: "", phone_number: ""})
+    }, [contacts])
+
+    const onChanceInput = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (form.fullname === "" || form.phone_number === "") {
+            return false;
+        }
+
+        addContacts([...contacts, form])  
+        
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+
+            <div>
+                <input name="fullname"
+                       placeholder="Fullname"
+                       value={form.fullname}
+                       onChange={onChanceInput}
+                />
+            </div>
+
+            <div>
+                <input name="phone_number"
+                       placeholder="Phone Number"
+                       value={form.phone_number}
+                       onChange={onChanceInput}
+                />
+            </div>
+
+            <div>
+                <button>Add</button>
+            </div>
+            
+        </form>
+    )
+}
+
+export default Form;
---
+++ src/components/Contacts/index.js
@@ -0,0 +1,22 @@
+import {useState, useEffect} from "react";
+
+import List from "./List";
+import Form from "./Form";
+
+function Contacts() {
+
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        console.log(contacts);
+    }, [contacts])
+
+    return(
+        <div>            
+            <List />
+            <Form addContacts={setContacts} contacts={contacts} />
+        </div>
+    )
+}
+
+export default Contacts
